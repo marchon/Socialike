2fcedb6f04cc50710dfd51c44e3a2367b29cc6d9
vinay babu <vinay.babu81@gmail.com>
2013-08-07 00:23:28 +0530

    Menu changes

--- js/custom.js
+++ js/custom.js
@@ -275,7 +275,6 @@ function homePage() {
                 msnry = new Masonry(container, {
                     // options
                     //isResizable: true,
-                    "isOriginLeft": false,
                     isAnimated: true,
                     columnWidth: 200,
                     "gutter": 6,
