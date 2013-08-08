7dcf3bc8514bbba55f05bfb76273a65ef22dae92
vinay babu <vinay.babu81@gmail.com>
2013-08-09 03:01:54 +0530

    Menu styling

--- js/custom.js
+++ js/custom.js
@@ -374,7 +374,7 @@ function highCharts(chartdat) {
             tooltip: {
                 backgroundColor: '#fff',
                 borderRadius: '50%',
-                borderWidth: 0,
+                borderWidth: 1,
                 shadow: false,
                 useHTML: true,
                 headerFormat: '<div class="tooltip_main"><div class="tooltip_location">{point.key}</div>',
