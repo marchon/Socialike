e17674abf410e99623b24a31ccd5121b028ba0c0
vinay babu <vinay.babu81@gmail.com>
2013-08-16 23:22:26 +0530

    new changes on Aug-16

--- js/fb.js
+++ js/fb.js
@@ -1,4 +1,4 @@
-  window.fbAsyncInit = function() {
+  window.fbAsyncInit = function () {
       FB.init({
           appId: '488181561252340', // App ID
           channelUrl: 'https://socialike.herokuapp.com/home.html', // Channel File
@@ -13,7 +13,7 @@
 
 
 
-      FB.Event.subscribe('auth.login', function(response) {
+      FB.Event.subscribe('auth.login', function (response) {
 
           if (response.status === 'connected') {
 
@@ -35,18 +35,18 @@
 
 
 
-      FB.getLoginStatus(function(response) {
+      FB.getLoginStatus(function (response) {
 
           if (response.status === 'connected') {
 
-            if (window.location.pathname == "/") {
-              window.top.location = "/home.html";
-              //homePage();
-            }else if(window.location.pathname == "/home.html")
-            {
+              if (window.location.pathname == "/") {
 
-              homePage();
-            }
+                  window.top.location = "/home.html";
+                  //homePage();
+              } else if (window.location.pathname == "/home.html") {
+
+                  homePage();
+              }
 
           } else if (response.status === 'not_authorized') {
               // the user is logged in to Facebook, 
@@ -67,19 +67,19 @@
       //     }
       // });
 
+  }
 
 
-
-   // Load the SDK asynchronously
-  (function(d) {
-      var js, id = 'facebook-jssdk',
-          ref = d.getElementsByTagName('script')[0];
-      if (d.getElementById(id)) {
-          return;
-      }
-      js = d.createElement('script');
-      js.id = id;
-      js.async = true;
-      js.src = "//connect.facebook.net/en_US/all.js";
-      ref.parentNode.insertBefore(js, ref);
-  }(document));
+      // Load the SDK asynchronously
+      (function (d) {
+          var js, id = 'facebook-jssdk',
+              ref = d.getElementsByTagName('script')[0];
+          if (d.getElementById(id)) {
+              return;
+          }
+          js = d.createElement('script');
+          js.id = id;
+          js.async = true;
+          js.src = "//connect.facebook.net/en_US/all.js";
+          ref.parentNode.insertBefore(js, ref);
+      }(document));
