a7aa526483669622cbd7522da5855046e6b6ccf8
vinay babu <vinay.babu81@gmail.com>
2013-08-16 23:30:59 +0530

    new changes on Aug-16

--- js/fb.js
+++ js/fb.js
@@ -1,4 +1,4 @@
-  window.fbAsyncInit = function () {
+  window.fbAsyncInit = function() {
       FB.init({
           appId: '488181561252340', // App ID
           channelUrl: 'https://socialike.herokuapp.com/home.html', // Channel File
@@ -13,7 +13,7 @@
 
 
 
-      FB.Event.subscribe('auth.login', function (response) {
+      FB.Event.subscribe('auth.login', function(response) {
 
           if (response.status === 'connected') {
 
@@ -24,7 +24,8 @@
           } else if (response.status === 'not_authorized') {
 
               // FB.login();
-              console.log('auth.login not autorised');
+              console.log('auth.login not authorized');
+
           } else {
 
               // FB.login();
@@ -35,51 +36,49 @@
 
 
 
-      FB.getLoginStatus(function (response) {
+      FB.getLoginStatus(function(response) {
 
-          if (response.status === 'connected') {
+          if (response.status === 'connected' ) {
 
-              if (window.location.pathname == "/") {
+             console.log('FB.getLoginStatus authorized');
 
-                  window.top.location = "/home.html";
-                  //homePage();
-              } else if (window.location.pathname == "/home.html") {
-
-                  homePage();
-              }
+              //homePage();
 
           } else if (response.status === 'not_authorized') {
               // the user is logged in to Facebook, 
               // but has not authenticated your app
               //fbLogin();
+              console.log('FB.getLoginStatus authorized');
+              
           } else {
               // the user isn't logged in to Facebook.
           }
       });
 
-      // FB.Event.subscribe('auth.statusChange', function(response) {
-      //     //do something with response
-      //     if (window.location.pathname == "/") {
+      FB.Event.subscribe('auth.statusChange', function(response) {
+          //do something with response
+          if (window.location.pathname == "/") {
 
-      //        console.log('auth.statusChange satisfied');
+              console.log('auth.statusChange satisfied');
 
-      //        window.top.location = "/home.html";
-      //     }
-      // });
-
-  }
-
-
-      // Load the SDK asynchronously
-      (function (d) {
-          var js, id = 'facebook-jssdk',
-              ref = d.getElementsByTagName('script')[0];
-          if (d.getElementById(id)) {
-              return;
+              window.top.location = "/home.html";
           }
-          js = d.createElement('script');
-          js.id = id;
-          js.async = true;
-          js.src = "//connect.facebook.net/en_US/all.js";
-          ref.parentNode.insertBefore(js, ref);
-      }(document));
+      });
+
+
+
+  };
+
+   // Load the SDK asynchronously
+  (function(d) {
+      var js, id = 'facebook-jssdk',
+          ref = d.getElementsByTagName('script')[0];
+      if (d.getElementById(id)) {
+          return;
+      }
+      js = d.createElement('script');
+      js.id = id;
+      js.async = true;
+      js.src = "//connect.facebook.net/en_US/all.js";
+      ref.parentNode.insertBefore(js, ref);
+  }(document));
