098a7a0c10981a685f648dd2c3ecbe84fad82201
vinay babu <vinay.babu81@gmail.com>
2014-10-16 22:58:25 +0530

    testing route

--- demo/app.js
+++ demo/app.js
@@ -45,7 +45,10 @@ angular.module('FbTest', ['ngRoute'])
 		FB.login(function(response) {
 
 			if (response.authResponse) {
-				$location.path('/friends');
+				FbService.getFriends(function(resp) {
+					//console.log(resp);
+					$scope.friends = resp;
+				});
         	}
 
 		});
