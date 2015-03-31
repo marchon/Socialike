61660934960dee75d64bc9c4a028aea190dd0396
Surendra <svsdesigns85@gmail.com>
2015-04-01 02:27:39 +0530

    adding compression module

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ console.log("starting");
 var port = process.env.PORT || 5000;
 process.env.PWD = process.cwd()
 
+var compression = require('compression');
+var express = require("express");
 
 var express = require("express");
 
 var app = express();
+
+app.use(compression());
 
 app.use(express.static(process.env.PWD));
 app.get("/", function(request, response) {
